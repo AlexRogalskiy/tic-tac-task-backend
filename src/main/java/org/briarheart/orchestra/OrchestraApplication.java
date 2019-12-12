d3e61f0388c7757d7755d5ff36ccf39cb295d6d7
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-12-12 14:01:53 +0300

    Experiments with deploying to PWS and cloud configuration

--- src/main/java/org/briarheart/orchestra/OrchestraApplication.java
+++ src/main/java/org/briarheart/orchestra/OrchestraApplication.java
@@ -1,11 +1,39 @@
 package org.briarheart.orchestra;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 public class OrchestraApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrchestraApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner projectNamePrinter(@Value("${application.name:n/a}") String projectName) {
+        return args -> {
+            System.out.println("Project name: " + projectName);
+        };
+    }
+
+    @Bean
+    public RefreshCounter refreshCounter() {
+        return new RefreshCounter();
+    }
+
+    public static class RefreshCounter {
+        private final AtomicLong counter = new AtomicLong(0L);
+
+        @EventListener
+        public void onRefresh(RefreshScopeRefreshedEvent e) {
+            System.out.println("Refreshed " + counter.incrementAndGet() + " times");
+        }
     }
 }
